b3c38f524087115c83864639d3244ab06078de51
Stulta-Amiko <phh01010312@gmail.com>
2024-01-17 17:06:52 +0900

    first commit

---
+++ src/cats.controller.ts
@@ -0,0 +1,18 @@
+/*import { Controller, Get } from '@nestjs/common';
+
+@Controller('cats')
+export class CatsController {
+  @Get()
+  findAll(): string {
+    return 'This action returns all cats';
+  }
+}*/
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('cats')
+export class CatsController {
+  @Get()
+  findAll(): string {
+    return 'This action returns all cats';
+  }
+}
---
+++ src/cats.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+
+/*@Injectable()
+export class CatsService {
+  constructor() {
+    this.cats = [];
+  }
+
+  create(cat) {
+    this.cats.push(cat);
+  }
+
+  findAll() {
+    return this.cats;
+  }
+}
+*/
